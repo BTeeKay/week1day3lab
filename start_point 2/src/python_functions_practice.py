f05a5cf2505db18317031c197645ffbb0cc5fb52
Cameron Wilson <cameronwilson@Camerons-MacBook-Air.local>
2022-05-04 15:09:41 +0100

    Hlafway Save

--- start_point 2/src/python_functions_practice.py
+++ start_point 2/src/python_functions_practice.py
@@ -6,3 +6,18 @@ def add(x, y):
 
 def subtract(x, y):
     return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return 10 / 2
+
+def length_of_string(x):
+    return len(x)
+
+def join_string(x, y):
+    return x + y
+
+def add_string_as_number(x, y):
+    return int(x) + int(y)
